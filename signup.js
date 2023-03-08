4d28a8997a86d595aed27d59214b8c7e8b619838
Erel Edree <erel2000@gmail.com>
2023-03-08 13:28:27 +0200

    added features, fixed form

--- signup.js
+++ signup.js
@@ -1,3 +1,14 @@
 const relForm = document.querySelector('form');
 const subBtn = document.querySelector('.submit-btn');
+const inputs = document.querySelectorAll('.main-form input')
+const pass = document.querySelector('#pass')
+const passConf = document.querySelector('#passConf')
+const passDiv = document.querySelector('.passDiv')
+const passErrMsg = document.querySelector('.passDiv>p')
 
+subBtn.addEventListener('click',(e)=>{
+    if(pass.value!==passConf.value){
+        e.preventDefault();
+        passErrMsg.classList.remove('hidden')
+    }
+})
